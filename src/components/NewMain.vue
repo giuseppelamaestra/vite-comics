d9b0c6992cd16ee47b1ea84781f7f81cfe23d770
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-16 20:08:19 +0200

    fixed main wrapper

--- src/components/NewMain.vue
+++ src/components/NewMain.vue
@@ -1,11 +1,13 @@
 <template>
  
  <section class="bg-black">
-  <div class="wrapper"></div>
+  <div class="wrapper">
     <h1>
         Content Goes Here 
     </h1>
 
+  </div>
+  
 </section>
 </template>
 <script></script>
