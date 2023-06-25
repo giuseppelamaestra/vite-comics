8342090eed11c7d46a7498d9f4e514a0aa6fbf2b
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-26 01:24:39 +0200

    Added prop but I have to fixed something

--- src/components/SingleComic.vue
+++ src/components/SingleComic.vue
@@ -1,9 +1,26 @@
-<template></template>
+<template>
+ <article>
+
+    <h1>
+        {{ ComicElement }}
+    </h1>
+ </article> 
+ 
+</template>
 <script>
 export default {
     name : 'SingleComic',
+    props: {
+       ComicElement : Object,
+    }
+       
+    
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+article {
+     width: calc(100% / 6);
+}
 </style>
